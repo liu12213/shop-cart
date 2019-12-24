8d6cee19a869256cca6add267a1295dd18b33ba5
yourname <875149571@qq.com>
2019-12-24 19:06:48 +0800

    12.24

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# shop-cart
+购物车
+liu12213.github.io/shop-cart
---
+++ src/constants/actionTypes.js
@@ -0,0 +1,12 @@
+
+
+export const RECEIVE_SIZE='RECEIVE_SIZE';
+export const RECEIVE_PRODUCTS='RECEIVE_PRODUCTS';
+export const SHOW_CART='SHOW_CART';
+export const BUY_PRODUCT_BUY_ID='BUY_PRODUCT_BUY_ID';
+export const ADD_TO_CART='ADD_TO_CART';
+export const SELECT='SELECT';
+export const HIGHEST_TO_LOWEST='HIGHEST_TO_LOWEST';
+export const LOWEST_TO_HIGHEST='LOWEST_TO_HIGHEST';
+export const SELECT_TYPE='SELECT_TYPE';
+export const SELECT_SIZE='SELECT_SIZE';
